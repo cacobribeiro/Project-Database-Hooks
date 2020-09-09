8f2dc805a31327c6fee21acf97a985bbde1f03c4
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-09-09 12:19:18 -0300

    Updates

---
+++ precache-manifest.ac02428073521ff53165ea9d37c233aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec038bb6cbebf385e5501ba8697a870",
+    "url": "/Project-Database-Hooks/index.html"
+  },
+  {
+    "revision": "e7c086203e253d840978",
+    "url": "/Project-Database-Hooks/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "71e36362dbeaf5a4a9ff",
+    "url": "/Project-Database-Hooks/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "e7c086203e253d840978",
+    "url": "/Project-Database-Hooks/static/js/2.fe1ec7a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Project-Database-Hooks/static/js/2.fe1ec7a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e36362dbeaf5a4a9ff",
+    "url": "/Project-Database-Hooks/static/js/main.73b98608.chunk.js"
+  },
+  {
+    "revision": "4e801e1542dc0c0e96d9",
+    "url": "/Project-Database-Hooks/static/js/runtime-main.67838070.js"
+  }
+]);
